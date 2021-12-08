9787160d4fb3d1b91eb7e3780ecc48933980aa36
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-07 21:55:29 -0500

    added stylesheet and inline styles to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,28 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
 
-function Header({ text }) {
-    return (
-        <header>
-            <div className="container">
-                <h2>{text}</h2>
-            </div>
-        </header>
-    )
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
 }
 
 Header.defaultProps = {
-    text: 'Feedback UI'
-}
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
 
 Header.propTypes = {
-    text: PropTypes.string
-}
+  text: PropTypes.string,
+};
 
-export default Header
-
+export default Header;
