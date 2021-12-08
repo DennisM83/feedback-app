90ffa30f21e42a7d23097bfd6cb1a18def9f85d5
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-07 22:14:37 -0500

    brought feedbackItem into App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header />
-      <div>
-        <h1 className="container">My App</h1>
+      <div className="container">
+        <FeedbackItem/>
       </div>
     </>
   );
