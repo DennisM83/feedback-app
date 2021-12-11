1ffd0131deb888807455f171e852e77edc8e1840
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-11 09:24:07 -0500

    created list / passed in data dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
 
 function App() {
-    const [Feedback, setFeedback] = useState()
+    const [Feedback, setFeedback] = useState(FeedbackData)
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem/>
+        <FeedbackList feedback={Feedback}/>
       </div>
     </>
   );
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,17 @@
+import FeedbackItem from "./FeedbackItem"
+
+function FeedbackList({ feedback }) {
+    if (!feedback || feedback < 1) {
+        return <p>No feedback yet</p>
+    }
+
+    return (
+        <div className="feedback-list">
+            {feedback.map(item => (
+                <FeedbackItem key={item.id} item={item}/>
+            ))}
+        </div>
+    )
+}
+
+export default FeedbackList
