d3d3ac0136d12b99f00ea6857fc994c30a7dc796
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-11 08:57:29 -0500

    created a file for the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
+    const [Feedback, setFeedback] = useState()
+
   return (
     <>
       <Header />
